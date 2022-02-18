c9390a6914c1298a16d29df07b84f0a957b7894d
Stefano Cerra <stefanocerra@MacBook-Pro-von-Stefano.local>
2022-02-18 15:39:41 +0100

    CHANGE: Created delete function

--- delete.php
+++ delete.php
@@ -1,1 +1,15 @@
 <?php
+    require 'connector.php';
+
+    $id = $_GET['id'];
+
+    $result = $mysqli->query("SELECT * FROM album where id_album = $id");
+    $row = mysqli_fetch_assoc($result);
+    $cover_file = $row['cover_file'];
+    unlink($cover_file);
+
+    $query = $mysqli->prepare("DELETE FROM album WHERE bild_id=?");
+    $query->bind_param('i', $id);
+    $query->execute();
+    header("Location: album.php");
+?>
