bdff22954ee70a8c1ebe2575444a3e6b98940bfc
Stefano Cerra <stefanocerra@MacBook-Pro-von-Stefano.local>
2022-02-18 11:54:35 +0100

    DEV

--- album.php
+++ album.php
@@ -1,55 +1,55 @@
-<?php
-require 'connector.php';
-
-$result = $mysqli->query("SELECT * FROM album");
-
-    while ($row = mysqli_fetch_assoc($result)) {
-
-        $folderid = $row['id_album'];
-        $covername = $row['cover_file'];
-        $path = "/uk-307/files/$folderid";
-        $songs_number = $row['number_songs'];
-
-        $album_id = $row['id_album'];
-        $query = $mysqli->prepare("SELECT * FROM songs WHERE fid_album = ? LIMIT 3");
-        $query->bind_param('i',$album_id);
-        $query->execute();
-        $result_song = $query->get_result();
-    }
-?>
 <!doctype html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="stylealbum.css">
+    <link rel="stylesheet" href=".css">
     <title>Meine Alben</title>
 </head>
 <body>
-    <div class="alleAlben">
+    <div class="AlleAlbenAlles">
         <div class="alleAlbenContent">
             <a href="index.php"><input type="button" value="Neues Album hinzufügen"></a>
         </div>
-        <div class="alleAlbenContent">
-            <h1>Meine Alben</h1>
-        </div>
-        <div class="alleAlbenContent">
-            <img src='<?="$path/$covername"?>' style='width: 400px'>
-        </div>
-        <div class="alleAlbenContent">
-            <?php
-            while ($row_song = mysqli_fetch_assoc($result_song)):
-                $songtitle = $row_song['song_file'];
-                ?>
-                <h3><?=$row_song['song_file']?></h3>
-                <audio controls>
-                    <source src="<?=$path . '/' . $row_song['song_file']?>">
-                </audio>
+        <div class="alleAlben">
+            <div class="alleAlbenContent">
+                <?php
+                    require 'connector.php';
 
-            <?php endwhile; ?>
-        </div>
-        <p><?=$songtitle?></p>
-        <div class="alleAlbenContent">
-            <a href="detail.php?id=<?=$album_id?>"><input type='button' value='Album ansehen'></a>
+                    $result = $mysqli->query("SELECT * FROM album");
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $folderid = $row['id_album'];
+                        $covername = $row['cover_file'];
+                        $path = "/uk-307/files/$folderid";
+                        $songs_number = $row['number_songs'];
+                        $album_id = $row['id_album'];
+                        $album_title = $row['titel'];
+                    ?>
+                        <div class="alleAlbenContent">
+                            <img src='<?="$path/$covername"?>' style='width: 400px'>
+                        </div>
+                        <div class="alleAlbenContent">
+                            <p><?=$album_title?></p>
+                        </div>
+                        <div class="alleAlbenContent">
+                            <a href="detail.php?id=<?=$album_id?>"><input type='button' value='Album ansehen'></a>
+                        </div>
+                    <?php
+                        $query = $mysqli->prepare("SELECT * FROM songs WHERE fid_album = ? LIMIT 3");
+                        $query->bind_param('i',$album_id);
+                        $query->execute();
+                        $result_song = $query->get_result();
+                        ?>
+                        <?php
+                        while ($row_song = mysqli_fetch_assoc($result_song)): ?>
+                            <h3><?=$row_song['song_file']?></h3>
+                            <audio controls>
+                                <source src="<?=$path . '/' . $row_song['song_file']?>">
+                            </audio>
+                        <?php endwhile;
+                    }
+                ?>
+            </div>
         </div>
     </div>
 </body>
--- detail.php
+++ detail.php
@@ -5,48 +5,53 @@
     <title>Album Detail</title>
 </head>
 <body>
-    <a href="album.php"><input type="button" value="Zurück"></a>
-    <br>
-    <br>
+    <div class="detail">
+        <div class="detailContent">
+            <a href="album.php"><input type="button" value="Zurück"></a>
+        </div>
+        <?php
+            require 'connector.php';
+
+            $id = $_GET['id'];
+
+            $query_album = $mysqli->prepare("SELECT * FROM album WHERE id_album = ?");
+            $query_album->bind_param('i',$id);
+            $query_album->execute();
+
+            $result = $query_album->get_result();
+
+            $row = mysqli_fetch_assoc($result);
+
+            $folderid = $id;
+            $covername = $row['cover_file'];
+            $path = "/uk-307/files/$folderid";
+            $songs_number = $row['number_songs'];
+        ?>
+
+            <div class="deteilContent">
+                <p><?=$row['titel']?></p>
+            </div>
+            <div class="deteilContent">
+                <p><?=$row['description']?></p>
+            </div>
+            <div class="deteilContent">
+                <img src=<?="$path/$covername"?> style='width: 400px'>
+            </div>
+        <?php
+            $query = $mysqli->prepare("SELECT * FROM songs WHERE fid_album = ?");
+            $query->bind_param('i',$id);
+            $query->execute();
+
+            $result_song = $query->get_result();
+
+            while ($row_song = mysqli_fetch_assoc($result_song)): ?>
+                <h3><?=$row_song['song_file']?></h3>
+                <audio controls>
+                    <source src="<?=$path . '/' . $row_song['song_file']?>">
+                </audio>
+                <br>
+            <?php endwhile;
+        ?>
+    </div>
 </body>
 </html>
-
-<?php
-    require 'connector.php';
-
-    $id = $_GET['id'];
-
-    $query_album = $mysqli->prepare("SELECT * FROM album WHERE id_album = ?");
-    $query_album->bind_param('i',$id);
-    $query_album->execute();
-
-    $result = $query_album->get_result();
-
-    $row = mysqli_fetch_assoc($result);
-
-    $folderid = $id;
-    $covername = $row['cover_file'];
-    $path = "/uk-307/files/$folderid";
-    $songs_number = $row['number_songs'];
-
-    echo "$row[titel] ";
-    echo "<br>";
-    echo "$row[description]";
-    echo "<br>";
-    echo "<img src='$path/$covername' style='width: 400px'>";
-    echo "<br>";
-
-    $query = $mysqli->prepare("SELECT * FROM songs WHERE fid_album = ?");
-    $query->bind_param('i',$id);
-    $query->execute();
-
-    $result_song = $query->get_result();
-
-    while ($row_song = mysqli_fetch_assoc($result_song)): ?>
-        <h3><?=$row_song['song_file']?></h3>
-        <audio controls>
-            <source src="<?=$path . '/' . $row_song['song_file']?>">
-        </audio>
-        <br>
-    <?php endwhile;
-
