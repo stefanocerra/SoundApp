7ef3cbcfccea0d3798e450a5b196c2373040cf85
Stefano Cerra <stefanocerra@MacBook-Pro-von-Stefano.local>
2022-02-24 08:55:32 +0100

    CHANGE: delete files

--- delete.php
+++ delete.php
@@ -3,22 +3,27 @@
 
     $id = $_GET['id'];
 
-    //$test = dir(unlink("/uk-307/files/$id"));
+    $result = $mysqli->query("SELECT * FROM album where id_album = $id");
+    $row = mysqli_fetch_assoc($result);
+    $cover_file = $row['cover_file'];
 
-    $test = '1';
+    $path = "files/$id";
+    $files = glob($path . '/*');
 
-    Select ausprobieren!!!
+    foreach($files as $file){
+        if(is_file($file)) {
+            unlink($file);
+        }
+    }
 
+    rmdir("$path");
 
-    $query = $mysqli->prepare("DELETE FROM album WHERE id_album = ?");
-    $query->bind_param('i', $test);
+    $query = $mysqli->prepare("DELETE FROM album WHERE id_album=?");
+    $query->bind_param('i', $id);
     $query->execute();
 
-    /*
     $query = $mysqli->prepare("DELETE FROM songs WHERE fid_album=?");
     $query->bind_param('i', $id);
     $query->execute();
-    header("Location: album.php");
-    /*
-?>
 
+    header("Location: album.php");
